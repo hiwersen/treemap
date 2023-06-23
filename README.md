710cbf1c2b98456e1f5cfe439b93fb160564d924
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-06-23 17:09:30 +0100

    feat: Implement client and server side code - all tests passed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # heatmap
-Heat Map - FreeCodeCamp Data Visualization Project. For information about the project requirements visit https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-heat-map.
+Choropleth Map - FreeCodeCamp Data Visualization Project. For information about the project requirements visit https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-choropleth-map.
--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -1,11 +1,199 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+    /**
+     * Create and render the empty SVG root when the page first loads
+     * while wait for the JSON files to download
+     */
+    const svgW = 960;
+    const svgH = 600;
+    const paddingW = 50; // SVG area
+    const paddingH = 30; // SVG area
+
+    const tooltip = d3.select("body")
+    .append("div")
+    .attr("id", "tooltip")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+    .style("position", "absolute")
+    .style("left", "-100px")
+    .style("top", "-100px");
+
+    const title = d3.select("#chart-container")
+    .append("h1")
+    .text("United States Educational Attainment")
+    .attr("id", "title");
+
+    const description = d3.select("#chart-container")
+    .append("p")
+    .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)")
+    .attr("id", "description");
+
+    const svg = d3.select("#chart-container")
+    .append("svg")
+    .attr("id", "svg")
+    .attr("width", svgW)
+    .attr("height", svgH);
+
+    const counties = svg
+    .append("g")
+    .attr("class", "counties");
+
+    const source = d3.select("#chart-container")
+    .append("div")
+    .attr("id", "source")
+    .html(`Source: <a href="https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx" target="_blank">USDA Economic Research Service</a>`);
+
+    
     const req = new XMLHttpRequest();
 
-    req.open("GET", url, true);
+    const urlCountyData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+    const urlEducationData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+
+    req.open("GET", urlCountyData, true);
+    //req.open("GET", urlEducationData, true);
     req.send();
     req.onload = () => {
-        const response = JSON.parse(req.responseText);
+        // Fetch topoJSON file
+        let { responseText: response } = req;
+
+        // Convert JSON string into JS Object
+        const usCounties = JSON.parse(response);
+
+        const features = topojson.feature(usCounties, usCounties.objects.counties).features;
+        
+        counties.selectAll("path")
+        .data(features)
+        .enter()
+        .append("path")
+        .attr("class", "county")
+        .attr("d", d3.geoPath());
+
+        req.open("GET", urlEducationData, true);
+        //req.open("GET", urlEducationData, true);
+        req.send();
+        req.onload = () => {
+            let { responseText: response } = req;
+
+            // Convert JSON string into JS Object
+            const usEducation = JSON.parse(response);
+
+            usEducation.forEach(element => {
+                element.education = element.bachelorsOrHigher;
+                delete element.bachelorsOrHigher;
+            });
+
+            const minEdu = d3.min(usEducation, ({ education }) => education);
+            const maxEdu = d3.max(usEducation, ({ education }) => education);
+
+            const colorScale = d3.scaleQuantize()
+            .domain([minEdu, maxEdu]) // both are inclusive
+            .range([
+                "#CEE8F0",
+                "#A9D6E5",
+                "#7DC1D8",
+                "#51ADCB",
+                "#3490AF",
+                "#276C83",
+                "#1A4858",
+            ]);
+
+            counties.selectAll("path")
+            .data(usEducation)
+            .attr("data-fips", ({ fips }) => fips)
+            .attr("data-education", ({ education }) => education)
+            .attr("fill", ({ education }) => colorScale(education))
+            .on("mouseover", d => {
+                tooltip
+                .html(`<p>${d.area_name}, ${d.state}: ${d.education}%</p>`)
+                .attr("data-education", d.education)
+                .style("opacity", 1)
+                .style("position", "absolute")
+                .style("top", event.pageY + "px")
+                .style("left", event.pageX + "px");
+            })
+            .on("mouseout", () => {
+                tooltip
+                .style("opacity", 0)
+                .style("top", "-100px")
+                .style("left", "-100px");
+            });
+
+            const legendW = 250;
+            const legendH = 10;
+            const legendCellW = legendW / colorScale.range().length;
+            
+    
+            const legend = svg.append("g")
+            .attr("id", "legend")
+            .attr("transform", `translate(${svgW / 2 + legendCellW * 3}, ${paddingH / 2})`);
+    
+            legend.selectAll("rect")
+            .data(colorScale.range())
+            .enter()
+            .append("rect")
+            .attr("x", (_, i) => i * legendCellW)
+            .attr("y", 0)
+            .attr("width", legendCellW)
+            .attr("height", legendH)
+            .attr("fill", d => d);
+
+            const legendScale = d3.scaleLinear()
+            .domain([minEdu / 100, maxEdu / 100])
+            .range([0, legendW]);
+
+            const getTickValues = (min, max, Categorynum) => {
+                let ticks = [];
+                const range = (max - min) / Categorynum;
+                let tick = min;
+
+                while (ticks.length <= Categorynum) { // The last tick value is inclusive
+                    ticks.push(Number.parseFloat(tick.toFixed(1)) / 100);
+                    tick += range;
+                }
+                return ticks;
+            };
+            
+
+            const legendAxisGenerator = d3.axisBottom(legendScale)
+            .tickValues(getTickValues(minEdu, maxEdu, colorScale.range().length))
+            .tickFormat(d3.format("0.0%"));
+
+            
+            const legendAxis = legend.append("g")
+            .attr("id", "legend-axis")
+            .attr("transform", `translate(0, ${legendH})`)
+            .call(legendAxisGenerator);
+
+            legendAxis.selectAll("g").attr("class", "tick");
+
+        };
+
+
+
+
+
+        /*
+        const drawRect = context => {
+            context.rect(140, 140, 140, 140);
+            return context.toString();
+        }
+
+        let rectangle = d3.path();
+        rectangle.rect(320, 140, 140, 140);
+
+        svg
+        .append("path")
+        .attr("stroke", "black")
+        .attr("fill", "yellow")
+        .attr("d", drawRect(d3.path()));
+
+        svg
+        .append("path")
+        .attr("stroke", "black")
+        .attr("fill", "navy")
+        .attr("d", rectangle.toString());
+        */
+
+        /*
         const { monthlyVariance: dataset } = response;
         const { baseTemperature } = response;
 
@@ -21,8 +209,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const minTemp = d3.min(dataset, d => d.temperature);
         const maxTemp = d3.max(dataset, d => d.temperature);
 
-        const svgW = 1200;
-        const svgH = 500;
+        const svgW = 960;
+        const svgH = 600;
         const paddingW = 80; // SVG area
         const paddingH = 60; // SVG area
         const domainPadding = 1; // 1 year
@@ -68,14 +256,6 @@ document.addEventListener("DOMContentLoaded", () => {
         .domain([minY, maxY])
         .range([svgH - paddingH - cellH, paddingH]);
 
-        const tooltip = d3.select("body")
-        .append("div")
-        .attr("id", "tooltip")
-        .attr("class", "tooltip")
-        .style("opacity", 0)
-        .style("position", "absolute")
-        .style("left", "-100px")
-        .style("top", "-100px");
 
         const svg = d3.select("#chart-container")
         .append("svg")
@@ -83,27 +263,28 @@ document.addEventListener("DOMContentLoaded", () => {
         .attr("width", svgW)
         .attr("height", svgH);
 
-        svg.selectAll("rect")
+        const counties = svg
+        .append("g")
+        .attr("class", "counties");
+        
+        counties.selectAll("path")
         .data(dataset)
         .enter()
-        .append("rect")
+        .append("path")
         .attr("x", d => xScale(d.year))
         .attr("y", d => yScale(d.month))
         .attr("width", cellW )
         .attr("height", cellH)
         .attr("fill", d => colorScale(d.temperature))
-        .attr("class", "cell")
-        .attr("data-year", d => d.year)
-        .attr("data-month", d => d.month)
-        .attr("data-temp", d => d.temperature)
+        .attr("class", "county")
+        .attr("data-fips", d => d.year)
+        .attr("data-education", d => d.month)
         .on("mouseover", (event, d) => {
             tooltip
             .html(`${d.year} - ${monthScale(d.month)}<br>
             Temperature: ${d.temperature.toFixed(1)}℃<br>
             Variance: ${d.variance.toFixed(1)}℃`)
-            .attr("data-year", d.year)
-            .attr("data-month", d.month)
-            .attr("data-temp", d.temperature)
+            .attr("data-education", d.year)
             .style("opacity", 1)
             .style("position", "absolute")
             .style("top", event.pageY + "px")
@@ -135,23 +316,6 @@ document.addEventListener("DOMContentLoaded", () => {
         xAxis.selectAll("g").attr("class", "tick");
         yAxis.selectAll("g").attr("class", "tick");
 
-        svg
-        .append("text")
-        .text("Monthly Global Land-Surface Temperature")
-        .attr("id", "title")
-        .attr("x", svgW / 2)
-        .attr("y", paddingH / 2)
-        .style("text-anchor", "middle")
-        .style("font-size", "1.5rem");
-
-        svg
-        .append("text")
-        .text(`${minX} - ${maxX}: base temperature ${baseTemperature}℃`)
-        .attr("id", "description")
-        .attr("x", svgW / 2)
-        .attr("y", (paddingH / 2) + 20)
-        .style("text-anchor", "middle")
-        .style("font-size", "0.9rem");
 
         const legendH = colorScale.range().length * cellH;
 
@@ -196,5 +360,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .call(legendAxisGenerator);
 
         legendAxis.selectAll("g").attr("class", "tick");
+        */
+    
     };
 });
